308e187f2e63d6fe45a77d5dd3708075bf0218bc
roee030 <roee.angel93@gmail.com>
2020-05-02 02:05:10 +0300

    add phone number and email

--- lib/main.dart
+++ lib/main.dart
@@ -12,42 +12,85 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.red[500],
+          backgroundColor: Colors.blue[500],
           title: Center(child: Text('Roee Angel')),
         ),
-        backgroundColor: Colors.red,
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        backgroundColor: Colors.blue,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              child: Text('Container1'),
-              color: Colors.orange,
-              height: double.infinity,
-              width: 100.0,
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/user_photo.jpg'),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: Text('container2'),
+            Text('Roee Angel',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    color: Colors.white)),
+            Text('Fullstack Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5)),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                height: 20,
+                color: Colors.white,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 35.0, vertical: 15.0),
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '054 833 6350',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.white,
-                  child: Text('container3'),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 35.0, vertical: 15.0),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Roee.angel93@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Container(
-              width: 100.0,
-              height: double.infinity,
-              color: Colors.blue,
-              child: Text('container2'),
-            ),
+              ),
+            )
           ],
         ),
       ),
